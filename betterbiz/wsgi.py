128acf6b88872eb892a92b9bfbe4909e0a371c60
bkj <brbojr@gmail.com>
2024-03-25 15:11:37 -0400

    environment updates

--- betterbiz/wsgi.py
+++ betterbiz/wsgi.py
@@ -11,9 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-if os.environ.get('DJANGO_ENV') == 'production':
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "betterbiz.settings.production")
-else:    
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "betterbiz.settings.development")
+# if os.environ.get('DJANGO_ENV') == 'production':
+#     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "betterbiz.settings.production")
+# else:    
+environment = os.getenv('DJANGO_ENV','development')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "betterbiz.settings.{environment}")
 
 application = get_wsgi_application()
