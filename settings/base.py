1521847a1ab0e0280ad583f2e22cf7e9fd6be935
bkj <brbojr@gmail.com>
2024-04-19 06:45:25 -0400

    frontend url added to CORS_ALLOWED_ORIGINS

--- settings/base.py
+++ settings/base.py
@@ -35,11 +35,13 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    ' http://localhost:5173'
+    ' http://localhost:5173',
+    'http://betterbiz.thelendingline.com'
 ] 
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'http://betterbiz.thelendingline.com'
 ]
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
--- settings/development.py
+++ settings/development.py
@@ -1,7 +1,8 @@
 from .base import *
+import dj_database_url
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DEBUG = os.environ.get('DEBUT','false') == True
+DEBUG = os.environ.get('DEBUG','false') == True
 SECRET_KEY = os.environ.get('SECRET_KEY',"django-insecure-w!#&qu$p-)d5$oqi7qx-n1k$-=6r=+f@-hn0t#@^^zs#qr8ahn")
 
 # SECURITY WARNING: don't run with debug turned on in production!
