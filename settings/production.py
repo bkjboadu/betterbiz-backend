c1fc17b5a5aa4cb253ef0c83f3bca794ae86cd44
bkj <brbojr@gmail.com>
2024-05-22 03:52:02 -0600

    Update database settings to use PostgreSQL

--- settings/production.py
+++ settings/production.py
@@ -11,4 +11,8 @@ DEBUG = True
 ALLOWED_HOSTS = ["betterbiz-ffe47fd6d1ed.herokuapp.com", "localhost", "127.0.0.1",]
 
 
-DATABASES = {"default": dj_database_url.config(default=os.environ.get("JAWSDB_URL"))}
+# DATABASES = {"default": dj_database_url.config(default=os.environ.get("JAWSDB_URL"))}
+
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
