53bb8621c9f685e9aeefbce5067ccab18ed6ee0d
bkj <brbojr@gmail.com>
2024-05-22 12:10:36 -0600

    questionnaire updated

--- questionnaire/api_views.py
+++ questionnaire/api_views.py
@@ -9,6 +9,8 @@ from business.models import Business,BusinessScore
 from rest_framework.exceptions import ValidationError
 from rest_framework_simplejwt.tokens import AccessToken
 from user_account.models import User
+from rest_framework.response import Response
+from django.utils import timezone
 
 
 
@@ -33,6 +35,19 @@ class QuestionViewSet(viewsets.ModelViewSet):
     serializer_class = QuestionSerializer
     permission_classes = []
     authentication_classes = []
+
+    def create(self,request,*args,**kwargs):
+        data = request.data
+
+        if Questions.objects.exists():
+            question_instance = Questions.objects.first()
+            question_instance.questions = data.get('questions',question_instance.questions)
+            question_instance.time = timezone.now()
+            question_instance.save()
+            serializers = self.get_serializer(question_instance)
+            return Response(serializers.data)
+        else:
+            return super().create(request,*args,**kwargs)
 
 class UserResponseViewSet(viewsets.ModelViewSet):
     queryset = UserResponse.objects.all()
@@ -88,154 +103,4 @@ class UserResponseViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(user_response)
         
         return Response(serializer.data,status=status.HTTP_201_CREATED)
-        
-        # serializer = self.get_serializer(data=request.data)
-        # if serializer.is_valid():
-        #     serializer.save(user=user,business=business)
-        #     return Response(serializer.data,status=status.HTTP_201_CREATED)
-        # else:
-        #     return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-#     @action(
-#         detail=True, methods=["GET"], permission_classes=[], authentication_classes=[]
-#     )
-#     def questions(self, request, pk=None):
-#         response_data = []
-#         industry = Industry.objects.get(pk=pk)
-#         base_categories = [category.name for category in industry.categories.all()]
-#         industry_serializer = IndustrySerializer(industry)
-#         base_categories.insert(1, industry.name + " specific")
-
-#         for category in base_categories:
-#             try:
-#                 category_object = Category.objects.get(name=category)
-#                 category_serializer = CategorySerializer(category_object)
-#                 response_data.append(category_serializer.data)
-#             except Category.DoesNotExist:
-#                 pass
-
-#         serialized_data = {
-#             "industry": industry_serializer.data,
-#             "categories": response_data,
-#         }
-
-#         return Response(serialized_data)
-
-#     @action(detail=True, methods=["GET", "POST"], url_path="add-category")
-#     def add_category_to_industry(self, request, pk=None):
-#         category_name = request.data.get("category_name", "")
-#         try:
-#             category = Category.objects.get(name=category_name)
-#         except (TypeError, ValueError, OverflowError, Category.DoesNotExist):
-#             category = None
-
-#         message = ""
-#         if category:
-#             industry = Industry.objects.get(pk=pk)
-#             industry.categories.add(category)
-#             industry.save()
-#             message = f"{category.name} Category added to {industry.name} Industry"
-#         print(message)
-#         return Response({"status": message})
-
-#     @action(
-#         detail=True, methods=["GET"], url_path="remove-category/(?P<category_id>[^/.]+)"
-#     )
-#     def remove_category_from_industry(self, request, pk=None, category_id=None):
-#         message = "bright"
-#         print(category_id, pk)
-#         try:
-#             category = Category.objects.get(id=category_id)
-#             industry = Industry.objects.get(id=pk)
-#             print(category, industry)
-#         except (
-#             TypeError,
-#             ValueError,
-#             OverflowError,
-#             Category.DoesNotExist,
-#             Industry.DoesNotExist,
-#         ) as e:
-#             print(e)
-#             return Response({"status": message})
-
-#         if category and industry:
-#             category.industries.remove(industry)
-#             message = f"{industry.name} Industry removed from Category {category.name} "
-#         return Response({"status": message})
-
-
-# class CategoryViewSet(viewsets.ModelViewSet):
-#     queryset = Category.objects.all()
-#     serializer_class = CategorySerializer
-#     permission_classes = []
-#     authentication_classes = []
-
-#     def get_permissions(self):
-#         if self.action in ["update", "partial_update", "destroy"]:
-#             permission_classes = [IsSuperUser]
-#         else:
-#             permission_classes = self.permission_classes
-#         return [permission() for permission in permission_classes]
-
-#     @action(detail=True, methods=["POST"], url_path="add-question")
-#     def add_question(self, request, pk=None):
-#         category = Category.objects.get(id=pk)
-#         data = request.data
-#         print(data)
-#         question = data.get("question", "")
-#         description = data.get("description", "")
-#         options = data.get("options", "")
-
-#         message = ""
-#         if question and category and (options is not None):
-#             question_object = SpecificQuestion.objects.create(
-#                 question=question, description=description, category_id=category.id
-#             )
-#             question_object.save()
-#             for option in options:
-#                 option_object = AnswerOption.objects.create(
-#                     option_text=option.get("option_text", ""),
-#                     points=option.get("points", ""),
-#                     question_id=question_object.id,
-#                 )
-#                 option_object.save()
-#             message = f"question and options has been created"
-
-#         return Response({"status": message})
-
-#     @action(
-#         detail=True, methods=["GET"], url_path="remove-question/(?P<question_id>[^/.]+)"
-#     )
-#     def remove_question(self, request, pk=None, question_id=None):
-#         question = SpecificQuestion.objects.get(id=question_id, category_id=pk)
-#         message = question.question
-#         question.delete()
-#         return Response({"status": message})
-
-#     @action(
-#         detail=True, methods=["POST"], url_path="edit-question/(?P<question_id>[^/.]+)"
-#     )
-#     def edit_question(self, request, pk=None, question_id=None):
-#         question = get_object_or_404(SpecificQuestion, pk=question_id)
-#         question_form = SpecificQuestionForm(request.data, instance=question)
-
-#         if question_form.is_valid():
-#             question_form.save()
-
-#             options = request.data.get("answer_options", "")
-#             for option_data in options:
-#                 option_id = option_data.get("id")
-#                 if option_id:
-#                     option = get_object_or_404(AnswerOption, pk=option_id)
-#                     option_form = AnswerOptionForm(option_data, instance=option)
-#                 else:
-#                     option_form = AnswerOptionForm(option_data)
-
-#                 if option_form.is_valid():
-#                     option_form.save()
-#                 else:
-#                     pass
-
-#             message = "Question and options updated"
-#         else:
-#             message = "Invalid data"
-#         return Response({"status": message})
+ 
--- questionnaire/models.py
+++ questionnaire/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from user_account.models import User
+from django.utils import timezone
 
 
 class Industry(models.Model):
@@ -15,6 +16,16 @@ class Industry(models.Model):
     
 class Questions(models.Model):
     questions = models.JSONField(null=True,blank=True)
+    time = models.DateTimeField(auto_now_add=True,null=True)
+
+    def save(self,*args,**kwargs):
+        if Questions.objects.exists():
+            existing = Questions.objects.first()
+            existing.questions = self.questions
+            existing.time = timezone.now()
+            super(Questions,existing).save(*args,**kwargs)
+        else:
+            super(Questions,self).save(*args,**kwargs)
 
 class UserResponse(models.Model):
     user = models.ForeignKey(User,related_name='response',on_delete=models.CASCADE)
