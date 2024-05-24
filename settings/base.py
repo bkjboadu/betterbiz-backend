c2704cf498375235da814bd1d56556ca0c22a5d3
bkj <brbojr@gmail.com>
2024-05-23 18:02:15 -0600

    heyhey

--- settings/base.py
+++ settings/base.py
@@ -61,8 +61,8 @@ SOCIALACCOUNT_PROVIDERS = {
         'APP': {
             'client_id': 'ABiMYuCAE8GQ2V417AnfP4hKtuU2d2VCx1oRgqz5LntaOaG4Z8',
             'secret': 'tZ3g3VYHignJKSaP47hBVx4fpgruZ3xuyrKJnWzw',
-            # 'redirect_uri': 'https://betterbiz-ffe47fd6d1ed.herokuapp.com/quickbooks/callback/',
-            'redirect_uri':"https://fa92-2604-3d09-6a77-4d00-c84a-7f02-602d-5b0d.ngrok-free.app/quickbooks/callback/" ,
+            'redirect_uri': 'https://betterbiz-ffe47fd6d1ed.herokuapp.com/quickbooks/callback/',
+            # 'redirect_uri':"https://fa92-2604-3d09-6a77-4d00-c84a-7f02-602d-5b0d.ngrok-free.app/quickbooks/callback/" ,
             'key': ''
         }
     }
